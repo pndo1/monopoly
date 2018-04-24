c7d921572724f3652234c3e392dbf29aa88988fe
Sunny Gandhi <sunny.gan90@gmail.com>
2018-04-24 10:39:24 -0500

    Initial Boxes Setup

--- Boxes.java
+++ Boxes.java
@@ -11,5 +11,39 @@
  */
 public class Boxes
 {
+    private String color;
+    private String type;
+    private String name;
+    private int value;
+    private int loc;
+    private Card property; //For later use
+    
+    public Boxes(String c, String t, String n, int v, int l){
+     color = c;
+     type = t;
+     name = n;
+     value = v;
+     loc = l;
+    }
+    
+    public String getColor(){
+        return color;
+    }
+    
+    public String getType(){
+        return type;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public int getValue(){
+        return value;
+    }
+    
+    public int getLoc(){
+        return loc;
+    }
 
 }
