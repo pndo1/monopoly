a9bfd5a1ca212eec6d596ee395eeabb4a148346e
Ash Backup <asriniv0@students.d125.org>
2018-04-25 11:02:05 -0500

    Modified ChestCard

--- ChestCard.java
+++ ChestCard.java
@@ -19,27 +19,34 @@ public class ChestCard extends Card
         {
             if(i==0){
                 super.setDescription("Advance to Go (Collect $200)");
+                super.setMove(0);
             }
             else if(i==1){
                 super.setDescription("Bank error in your favor – Collect $200");
+                super.setMoney(200);
             }
             else if(i==2){
                 super.setDescription("You have won second prize in a beauty contest – Collect $10");
+                super.setMoney(10);
             }
             else if(i==3){
                 super.setDescription("Doctor's fees {fee} – Pay $50");
+                super.setMoney(-50);
             }
             else if(i==4){
                 super.setDescription("From sale of stock you get $50");
+                super.setMoney(50);
             }
             else if(i==5){
                 super.setDescription("Get Out of Jail Free ");
             }
             else if(i==6){
                 super.setDescription("Go to Jail – Go directly to jail – Do not pass Go – Do not collect $200 ");
+                super.setMove(30);
             }
             else if(i==7){
                 super.setDescription("Grand Opera Night {Opening} – Collect $50 from every player for opening night seats");
+                
             }
             else if(i==8){
                 super.setDescription("You inherit $100");
@@ -64,7 +71,6 @@ public class ChestCard extends Card
             }
             else if(i==15){
                 super.setDescription("You are assessed for street repairs – $40 per house – $115 per hotel ");
-
             }   
         }
     }
