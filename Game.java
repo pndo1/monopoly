2ec16591769783e6befd0ad43b7e60b1fe538565
Ryan Mitchell <ryan@mitchx.org>
2018-05-01 10:51:18 -0500

    Added new method to Player

--- Game.java
+++ Game.java
@@ -1,24 +1,21 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-public class Game
-{
-    public static void main(String [] args)
-    {
-        Board board=new Board();
-        Scanner scan=new Scanner(System.in);
+
+public class Game {
+    public static void main(String[] args) {
+        Board board = new Board();
+        Scanner scan = new Scanner(System.in);
         System.out.print("How many players? ");
-        int numPlayers=scan.nextInt();
-        ArrayList<Player> players=new ArrayList<Player>();
-        for(int i=0;i<numPlayers;i++) {
+        int numPlayers = scan.nextInt();
+        ArrayList<Player> players = new ArrayList<Player>();
+        for (int i = 0; i < numPlayers; i++) {
             players.add(new Player());
         }
-        for(int i=0;i<11;i++) {
-            System.out.println(rollDice());
-        }
+        System.out.println("Ready player 1");
     }
 
 
-public static int rollDice() {
-    return (int)(Math.random()*6+1);
-}
+    public static int rollDice() {
+        return (int) (Math.random() * 6 + 1);
+    }
 }
